917833a437d5142ae0e39f4ef4c4aa538fd24aff
song <alr_song@qq.com>
2018-04-23 17:16:06 +0800

    1

--- src/components/home.vue
+++ src/components/home.vue
@@ -129,3 +129,31 @@
 	</div>
 </div>
 </template>
+<script type="text/javascript">
+	export default {
+	  data () {
+	    return {
+	      loading: false,
+	      post: null,
+	      error: null
+	    }
+	  },
+	  created () {
+	    // 组件创建完后获取数据，
+	    // 此时 data 已经被 observed 了
+	    this.fetchData()
+	  },
+	  watch: {
+	    // 如果路由有变化，会再次执行该方法
+	    '$route': 'fetchData'
+	  },
+	  methods: {
+	    fetchData () {
+	      this.error = this.post = null
+	      this.loading = true
+	      // replace getPost with your data fetching util / API wrapper
+	      console.log(1)
+	    }
+	  }
+	}
+</script>
